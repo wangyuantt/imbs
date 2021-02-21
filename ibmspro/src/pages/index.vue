5b325885f13c4251f6abfdc38fca731ad49d904e
wangyuan <wonyuan168@outlook.com>
2021-02-21 15:15:46 +0800

    配置首页路由

---
+++ ibmspro/src/pages/index.vue
@@ -0,0 +1,21 @@
+/*
+ * @Author: Wang Yuan 
+ * @Date: 2021-02-21 14:43:38 
+ * @Last Modified by: Wang Yuan
+ * @Last Modified time: 2021-02-21 14:58:57
+ */
+<template>
+    <div class="IMBS_APP APP"></div>
+</template>
+
+<script>
+export default {
+    name: "Index",
+    data () {
+        return {
+            name: "Index"
+        }
+    }
+}
+</script>
+
--- ibmspro/src/router/index.js
+++ ibmspro/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// components
+import Index from '@/pages/index'
 
 Vue.use(Router)
 
@@ -8,8 +9,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      alias: '/index',
+      name: 'Index',
+      component: Index
     }
   ]
 })
