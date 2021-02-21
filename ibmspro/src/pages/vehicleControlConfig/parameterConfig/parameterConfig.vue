ae32d731c5985b77a4f2df0fd2b3f6997a3a6865
wangyuan <wonyuan168@outlook.com>
2021-02-21 22:49:30 +0800

    新增组件

---
+++ ibmspro/src/pages/vehicleControlConfig/parameterConfig/parameterConfig.vue
@@ -0,0 +1,18 @@
+/*
+ * @Author: Wang Yuan 
+ * @Date: 2021-02-21 17:40:20 
+ * @Desc: 车辆管控配置-停车场-参数配置 
+ * @Last Modified by: Wang Yuan
+ * @Last Modified time: 2021-02-21 17:42:28
+ */
+<template>
+    <div class="parameter-config">
+        parameter-config
+    </div>
+</template>
+<script>
+export default {
+    name: 'paramterConfig'
+}
+</script>
+
---
+++ ibmspro/src/pages/vehicleControlConfig/vehicleControlConfig.vue
@@ -0,0 +1,19 @@
+/*
+ * @Author: Wang Yuan 
+ * @Date: 2021-02-21 17:35:17
+ * @Desc: 车辆管控配置
+ * @Last Modified by: Wang Yuan
+ * @Last Modified time: 2021-02-21 17:41:51
+ */
+<template>
+    <div class="vehicle-control-config">
+        vehicleControlConfig
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'vehicleControlConfig'
+}
+</script>
+
