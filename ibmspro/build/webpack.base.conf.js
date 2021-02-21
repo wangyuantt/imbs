2cf8ff84705de8447f74afc2557d6fb8da0476cf
wangyuan <wonyuan168@outlook.com>
2021-02-21 22:48:12 +0800

    配置路径

--- ibmspro/build/webpack.base.conf.js
+++ ibmspro/build/webpack.base.conf.js
@@ -27,6 +27,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      '@pages': resolve('src/pages'),
+      '@components': resolve('src/components')
     }
   },
   module: {
