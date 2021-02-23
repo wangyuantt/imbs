c8606175864a44146c0c80dd770fb0bfc5c55da4
wangyuan <wonyuan168@qq.com>
2021-02-23 19:13:19 +0800

    增加发卡设备参数

--- ibmspro/src/pages/vehicleControlConfig/parameterConfig/parameterConfig.vue
+++ ibmspro/src/pages/vehicleControlConfig/parameterConfig/parameterConfig.vue
@@ -3,7 +3,7 @@
  * @Date: 2021-02-21 17:40:20 
  * @Desc: 车辆管控配置-停车场-参数配置 
  * @Last Modified by: Wang Yuan
- * @Last Modified time: 2021-02-23 18:30:34
+ * @Last Modified time: 2021-02-23 18:51:35
  */
 <template>
     <div class="parameter-config">
@@ -24,6 +24,7 @@
                 <releaseParam v-if="itemActive === 0" />
                 <paymentParam v-if="itemActive === 1"  />
                 <alipayWeChatParam v-if="itemActive === 2" />
+                <cardIssuingDeviceParam v-if="itemActive === 3" />
             </div>
         </div>
     </div>
@@ -32,13 +33,17 @@
 import releaseParam from './components/releaseParam' // 放行参数
 import paymentParam from './components/paymentParam' // 缴费参数
 import alipayWeChatParam from './components/alitenConetParam' // 支付宝/微信对接参数
+import cardIssuingDeviceParam from './components/cardDeviceParam' // 发卡设备参数
+
+
 
 export default {
     name: 'parameterConfig',
     components: {
         releaseParam,
         paymentParam,
-        alipayWeChatParam
+        alipayWeChatParam,
+        cardIssuingDeviceParam
     },
     data () {
         return {
