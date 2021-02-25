84298000db2a9da859425e192380f27f21229f54
wangyuan <wonyuan168@qq.com>
2021-02-25 15:38:25 +0800

    添加注册

--- ibmspro/src/pages/login.vue
+++ ibmspro/src/pages/login.vue
@@ -2,11 +2,11 @@
  * @Author: Wang Yuan 
  * @Date: 2021-02-22 17:52:33 
  * @Last Modified by: Wang Yuan
- * @Last Modified time: 2021-02-25 15:00:51
+ * @Last Modified time: 2021-02-25 15:35:44
  */
 <template>
     <div class="login full-page flex-center">
-        <div class="login-action"  v-loading="formLoading" element-loading-text="登录中...">
+        <div class="login-action"  v-loading="formLoading" element-loading-text="加载中...">
             <el-row>
                 <el-col :span="23">
                     <el-form status-icon ref="ruleForm" label-width="100px" class="demo-ruleForm">
@@ -17,13 +17,13 @@
                             <el-input type="password" v-model="pass" autocomplete="off" placeholder="请输入密码"></el-input>
                         </el-form-item>
                         <el-form-item>
-                            <el-button style="width:100%" type="primary" @click="submitForm()">登录</el-button>
+                            <el-button style="width:100%" type="primary" @click="submitForm()">{{submitText}}</el-button>
                         </el-form-item>
                     </el-form>
                 </el-col>
-                <!-- <el-col :span='23' style="text-align: end;padding-right:20px">
-                    <el-link type="primary">去注册</el-link>
-                </el-col> -->
+                <el-col :span='23' style="text-align: end;padding-right:20px">
+                    <el-link type="primary" @click="switchType">{{actionText}}</el-link>
+                </el-col>
             </el-row>
         </div>
     </div>
@@ -33,13 +33,29 @@ export default {
     name: 'Login',
     data () {
         return {
+            showRegister: true,
             username: '',
             pass: '',
             formLoading: false
         }
     },
+    computed: {
+        submitText () {
+            return this.showRegister ? '登录' : '注册'
+        },
+        actionText () {
+            return this.showRegister ? '去注册' : '去登录'
+        }
+    },
     methods: {
         submitForm () {
+            if (!this.showRegister) {
+                this.registerAction()
+            } else {
+                this.loginAction()
+            }
+        },
+        loginAction () {
             this.formLoading = true
             let data = {
                 "mobile": this.username,
@@ -52,6 +68,30 @@ export default {
                     this.$router.push({
                         path: '/imbsPlatform/informationQuery'
                     })
+                } else {
+                    this.$message.error(res.msg)
+                }
+            })
+        },
+        switchType () {
+            this.showRegister = !this.showRegister
+        },
+        registerAction () {
+            this.formLoading = true
+            let data = {
+                "mobile": this.username,
+                "password": this.pass
+            }
+            this.$vueHttp.post('/app/register', data).then(res => {
+                this.formLoading = false
+                if (res.code === 0) {
+                    this.$message({
+                        message: '注册成功！请登录',
+                        type: 'success'
+                    })
+                    this.username = ''
+                    this.pass = ''
+                    this.showRegister = true
                 } else {
                     this.$message.error(res.msg)
                 }
