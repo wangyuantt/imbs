71e3abc095556d59cf9d2ddbc083f280cf526f54
wangyuan <wonyuan168@qq.com>
2021-02-26 21:22:33 +0800

    访客权限下载记录

--- ibmspro/src/api/onePassAPI/visitorManagementApi.js
+++ ibmspro/src/api/onePassAPI/visitorManagementApi.js
@@ -3,19 +3,19 @@
  * @Date: 2021-02-25 15:45:31
  * @Desc: 一卡通--访客管理模块  
  * @Last Modified by: Wang Yuan
- * @Last Modified time: 2021-02-26 19:40:22
+ * @Last Modified time: 2021-02-26 20:30:15
  */
 import * as IMBS_SERVER_API from '../interface'
 
-// 访客权限下载记录
-export const userLogin = (params = {}) => {
-    return IMBS_SERVER_API.post("/login/test", params)
-}
 // 访客预约记录
 export const appointmentRecords = (params = {}) => {
     return IMBS_SERVER_API.post("/hikisecurecenter/appointment/records", params)
 }
 // 来访记录
 export const visitRecord = (params = {}) => {
-    return IMBS_SERVER_API.post("hikisecurecenter/visiting/records", params)
+    return IMBS_SERVER_API.post("/hikisecurecenter/visiting/records", params)
+}
+// 访客权限下载记录
+export const visitorPerDownRecQry = (params = {}) => {
+    return IMBS_SERVER_API.post("/hikisecurecenter/auth/result", params)
 }
--- ibmspro/src/pages/onePass/visitorManagement/visitorpermissionDownloadRecord/visitorpermissionDownloadRecord.vue
+++ ibmspro/src/pages/onePass/visitorManagement/visitorpermissionDownloadRecord/visitorpermissionDownloadRecord.vue
@@ -1,8 +1,9 @@
 /*
  * @Author: Wang Yuan 
- * @Date: 2021-02-24 23:23:42 
+ * @Date: 2021-02-24 23:23:42
+ * @Desc: 访客权限下载记录   
  * @Last Modified by: Wang Yuan
- * @Last Modified time: 2021-02-24 23:45:08
+ * @Last Modified time: 2021-02-26 21:22:05
  */
 <template>
     <div class="visitor-permission-download-record vpdr">
@@ -22,6 +23,7 @@
                         <div class="tip-span margin-top-bottom">登记时间</div>
                         <el-date-picker
                             style="width:100%"
+                            @change="choseCheckinTime"
                             v-model="checkInTime"
                             type="datetime"
                             size="small"
@@ -42,21 +44,31 @@
                         </el-select>
                     </div>
                 </el-col>
+                <el-col :span="6" style="margin-top:40px">
+                    <div class="condition-three condition">
+                        <el-button type="primary" size='small' @click="queryBtnAction" :disabled="queryBtnSttus">查询</el-button>
+                        <el-button  size='small' @click="clearAllCondition">重置</el-button>
+                    </div>
+                </el-col>
             </el-row>
         </div>
         <div class="table">
             <el-row type="flex" justify="center">
                 <el-col :span=23>
-                    <el-table :data="tableData" border style="width: 100%" height="600" max-height='600'>
-                        <el-table-column prop="tLicenseNum" label="姓名"></el-table-column>
-                        <el-table-column prop="tParkingLot" label="登记时间"></el-table-column>
-                        <el-table-column prop="tParkingNumber" label="预计离开时间"></el-table-column>
-                        <el-table-column prop="tParkingStatus" label="授权类型"></el-table-column>
-                        <el-table-column prop="tLeaveTime" label="授权号码"></el-table-column>
-                        <el-table-column prop="tFloor" label="设备"></el-table-column>
-                        <el-table-column prop="tFloor" label="资源点名称"></el-table-column>
-                        <el-table-column prop="tFloor" label="被访对象"></el-table-column>
-                        <el-table-column prop="tFloor" label="权限状态"></el-table-column>
+                    <el-table :data="tableData" v-loading="tableLoading" element-loading-text="正在查询..." border style="width: 100%" height="600" max-height='600'>
+                        <el-table-column prop="visitorName" label="姓名"></el-table-column>
+                        <el-table-column prop="" label="登记时间"></el-table-column>
+                        <el-table-column prop="" label="预计离开时间"></el-table-column>
+                        <el-table-column prop="authType" label="授权类型">
+                            <template slot-scope="scope">
+                                <span>{{ scope.row.authType | authType}}</span>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="authNo" label="授权号码"></el-table-column>
+                        <el-table-column prop="deviceDesc" label="设备"></el-table-column>
+                        <el-table-column prop="doorDesc" label="资源点名称"></el-table-column>
+                        <el-table-column prop="" label="被访对象"></el-table-column>
+                        <el-table-column prop="authStatus" label="权限状态"></el-table-column>
                         <el-table-column prop="tFloor" label="操作"></el-table-column>
                     </el-table>
                 </el-col>
@@ -69,10 +81,10 @@
                         @size-change="handleSizeChange"
                         @current-change="handleCurrentChange"
                         :current-page="currentPage"
-                        :page-sizes="[100, 200, 300, 400]"
-                        :page-size="100"
+                        :page-sizes="[20, 40, 80, 100]"
+                        :page-size="pageSize"
                         layout="total, sizes, prev, pager, next, jumper"
-                        :total="4000">
+                        :total="total">
                     </el-pagination>
                 </el-col>
             </el-row>
@@ -86,15 +98,43 @@ export default {
         return {
             userName: '',
             checkInTime: '',
-            authorizationType: '0',
+            authorizationType: '',
             authorizationTypeOptions: [
                 {
-                    value: '0',
+                    value: '',
                     label: '全部'
+                },
+                {
+                    value: 0,
+                    label: '身份证'
+                },
+                {
+                    value: 1,
+                    label: '身份证序列号'
+                },
+                {
+                    value: 2,
+                    label: 'IC卡号'
+                },
+                {
+                    value: 3,
+                    label: '二维码'
+                },
+                {
+                    value: 4,
+                    label: '车牌号'
+                },
+                {
+                    value: 5,
+                    label: '人脸'
                 }
             ],
+            queryBtnSttus: false,
             tableData: [],
-            currentPage: 1
+            currentPage: 1,
+            pageSize: 20,
+            total: 0,
+            tableLoading: false
         }
     },
     computed: {
@@ -103,12 +143,54 @@ export default {
             return meta.subTitle + ' > ' + meta.currentPageTitle
         }
     },
+    mounted () {
+        this.getTableList()
+    },
     methods: {
+        queryBtnAction () {
+            this.queryBtnSttus = true
+            this.getTableList()
+        },
+        getTableList () {
+            this.tableLoading = true
+            let data = {
+                "orderId": "", // true 访客记录id，从查询访客来访记录v2接口获取返回参数中的orderId
+                "pageNo": this.currentPage,
+                "pageSize": this.pageSize
+            }
+            this.$visitorManagementAPI.visitorPerDownRecQry(data).then(res => {
+                this.queryBtnSttus = false
+                this.tableLoading = false
+                if (res.code === 0) {
+                    if (res.data.code == '0') {
+                        this.total = res.data.data.total
+                        this.tableData = res.data.data.list
+                    } else { 
+                        this.tableData = []
+                        this.$message.error(res.data.message) 
+                    }
+                } else {
+                    this.tableData = []
+                    this.$message.error(res.msg)
+                }
+            })
+        },
+        choseCheckinTime () {
+            let _startTime = this.$moment(this.checkInTime).format()
+            this.checkInTime = _startTime
+        },
         handleSizeChange(val) {
-            console.log(`每页 ${val} 条`);
+            this.pageSize = val
+            this.getTableList()
         },
         handleCurrentChange(val) {
-            console.log(`当前页: ${val}`);
+            this.currentPage = val
+            this.getTableList()
+        },
+        clearAllCondition () {
+            this.userName = ''
+            this.checkInTime = ''
+            this.authorizationType = ''
         }
     }
 }
