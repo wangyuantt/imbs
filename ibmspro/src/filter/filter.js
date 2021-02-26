928f555df52747a841a876bfe9ca2fe9bbc10209
wangyuan <wonyuan168@qq.com>
2021-02-26 21:24:34 +0800

    访客权限下载记录 -- 授权类型，设备类型

--- ibmspro/src/filter/filter.js
+++ ibmspro/src/filter/filter.js
@@ -2,7 +2,7 @@
  * @Author: Wang Yuan 
  * @Date: 2021-02-25 21:57:25 
  * @Last Modified by: Wang Yuan
- * @Last Modified time: 2021-02-26 20:11:10
+ * @Last Modified time: 2021-02-26 21:24:16
  */
 import moment from 'moment'
 
@@ -15,16 +15,16 @@ export const vehicleType = status => {
     switch (status) {
         case 0:
             return '其他车'
-            break;
+             
         case 1:
             return '小型车'
-            break;
+             
         case 2:
             return '大型车'
-            break;
+             
         case 3:
             return '摩托车'
-            break;
+             
     }
 }
 // 放行结果
@@ -32,13 +32,13 @@ export const releaseResult = status => {
     switch (status) {
         case 0:
             return '未放行'
-            break;
+             
         case 1:
             return '正常放行'
-            break;
+             
         case 2:
             return '离线放行'
-            break;
+             
     }
 }
 // 放行方式
@@ -46,16 +46,16 @@ export const releaseWay = status => {
     switch (status) {
         case 10:
             return '未开闸'
-            break;
+             
         case 11:
             return '自动开闸'
-            break;
+             
         case 12:
             return '人工/人工开闸'
-            break;
+             
         case 13:
             return '遥控器开闸'
-            break;
+             
     }
 }
 // 过车方向
@@ -63,10 +63,10 @@ export const vehicleOut = status => {
     switch (status) {
         case 0:
             return '进场'
-            break;
+             
         case 1:
             return '出场'
-            break;
+             
     }
 }
 // 交易方式
@@ -74,13 +74,13 @@ export const tradeWay = status => {
     switch (status) {
         case 1:
             return '现金'
-            break;
+             
         case 4:
             return '支付宝'
-            break;
+             
         case 5:
             return '微信'
-            break;
+             
     }
 }
 // 交易类型
@@ -88,10 +88,10 @@ export const tradeType = status => {
     switch (status) {
         case 0:
             return '充值'
-            break;
+             
         case 1:
             return '退款'
-            break;
+             
     }
 }
 // 性别
@@ -99,10 +99,8 @@ export const gender = status => {
     switch (status) {
         case 1:
             return '男'
-            break;
         case 2:
             return '女'
-            break;
     }
 }
 // 访客管理 > 1-3为：访客预约--访客状态，5-8为：来访距记录 -- 访客状态
@@ -110,24 +108,49 @@ export const visitorStatus = status => {
     switch (status) {
         case 1:
             return '正常'
-            break;
         case 2:
             return '迟到'
-            break;
         case 3:
             return '失效'
-            break;
         case 5:
             return '超期自动签离'
-            break;
         case 6:
             return '已签离'
-            break;
         case 7:
             return '超期未签离'
-            break;
         case 8:
             return '已到达'
-            break;
+    }
+}
+// 访客权限下载记录 -- 授权类型
+export const 人脸 = status => {
+    switch (status) {
+        case 0:
+            return '身份证'
+        case 1:
+            return '身份证序列号'
+        case 2:
+            return 'IC卡号'
+        case 3:
+            return '身份证'
+        case 4:
+            return '车牌号'
+        case 5:
+            return '人脸'
+    }
+}
+// 访客权限下载记录 -- 设备类型
+export const deviceType = status => {
+    switch (status) {
+        case 1:
+            return '门禁'
+        case 2:
+            return '梯控'
+        case 3:
+            return '可视对讲'
+        case 4:
+            return '停车场'
+        case 5:
+            return '人脸监控'
     }
 }
